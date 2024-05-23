aa16e5efd0272aa20ceedbef4cd1d6ff102a8db1
Yeramala Dherandar Reddy <142140331+DherandarReddy@users.noreply.github.com>
2024-05-23 14:05:03 +0530

    Update index.js

--- src/components/ActiveEventRegistrationDetails/index.js
+++ src/components/ActiveEventRegistrationDetails/index.js
@@ -83,3 +83,57 @@ const ActiveEventRegistrationDetails = props => {
 }
 
 export default ActiveEventRegistrationDetails
+
+/*import './index.css'
+
+const ActionEventRegistrationDetails = props => {
+  const {Status} = props
+  switch (Status) {
+    case 'YET_TO_REGISTER':
+      return (
+        <div className="ActionContainer">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/events-register-img.png"
+            className="ytrClass"
+            alt="yet to register"
+          />
+          <p>
+            A live performance brings so much to your relationship with dance
+          </p>
+          <button>Register Here</button>
+        </div>
+      )
+    case 'REGISTERED':
+      return (
+        <div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/events-regestered-img.png "
+            className="ytrClass"
+            alt="registered"
+          />
+          <h1>You have already registered for the event</h1>
+        </div>
+      )
+    case 'REGISTRATIONS_CLOSED':
+      return (
+        <div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/events-registrations-closed-img.png"
+            alt="registrations closed"
+            className="ytrClass"
+          />
+          <h1>Registrations Are Closed Now!</h1>
+          <p>Stay tuned. We will reopen</p>
+        </div>
+      )
+    default:
+      return (
+        <div>
+          <p>Click on an event, to view its registration details</p>
+        </div>
+      )
+  }
+}
+
+export default ActionEventRegistrationDetails
+*/
